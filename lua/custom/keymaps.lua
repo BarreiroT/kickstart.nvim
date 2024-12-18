88210dc8601b5a80e72ac8623f9e5d3939172153
BarreiroT <tomasmbarreiroi@gmail.com>
2024-12-18 12:42:52 -0300

    Refactor custom keymaps

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -1,1 +1,13 @@
+require 'custom.telescope_keymaps'
+require 'custom.oil_keymaps'
+
 vim.keymap.set('n', '<C-f>', '<cmd>silent !tmux neww tmux-sessionizer<CR>')
+
+vim.keymap.set('n', '<leader>on', '<cmd>lua require("custom.notes").open_notes_sidebar()<CR>', { noremap = true, silent = true, desc = '[O]pen [N]otes' })
+
+vim.api.nvim_set_keymap(
+  'n',
+  '<leader>obn',
+  '<cmd>lua require("custom.notes").open_or_create_branch_note()<CR>',
+  { noremap = true, silent = true, desc = '[O]pen [B]ranch [N]otes' }
+)
